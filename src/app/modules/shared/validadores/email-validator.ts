e00390ed98eee4ffeda426e9ecb4ee370bbf2066
Nycolas R. Alves <nycolasramon3@gmail.com>
2024-11-14 15:41:32 -0300

    Ajuste para botão secundário

--- src/app/modules/shared/validadores/email-validator.ts
+++ src/app/modules/shared/validadores/email-validator.ts
@@ -1,20 +1,11 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-/**
- * Validador personalizado para E-mails
- * @returns Função validadora de E-mails personalizada
- */
 export function validarEmail(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    // Obtendo o valor digitado pelo usuário
     const email = control.value;
-
-    // Expressão regular usada para validar se o valor digitado possui o
-    // formato básico de um e-mail, com @ e um ponto.
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    // Retornando um ValidationErrors com o atributo emailInvalido em TRUE
-    // caso o email possua um formato inválido e NULL, caso o formato do email esteja correto.
-    return email && !regex.test(email) ? { emailInvalido: true } : null;
+    const existeEmail = !!email;
+    const emailFormatoInvalido = !regex.test(email);
+    return existeEmail && emailFormatoInvalido ? { emailInvalido: true } : null;
   };
 }
